cdae11781320cc3d555cbd05b63a55cf2d38770d
Zaki-7 <61559275+Zaki-7@users.noreply.github.com>
2022-04-18 02:29:19 +0000

    zaki

--- src/main/java/com/zr7/compteservice/web/CompteRestController.java
+++ src/main/java/com/zr7/compteservice/web/CompteRestController.java
@@ -1,4 +1,39 @@
 package com.zr7.compteservice.web;
 
+import com.zr7.compteservice.entities.Compte;
+import com.zr7.compteservice.repositories.CompteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class CompteRestController {
+
+    private CompteRepository compteRepository;
+
+    public CompteRestController(CompteRepository compteRepository){
+        this.compteRepository = compteRepository;
+    }
+
+    @GetMapping(path = "/comptes")
+    public List<Compte> listComptes() {
+        return compteRepository.findAll();
+    }
+
+    @PostMapping(path = "/comptes")
+    public Compte save(@RequestBody Compte compte) {
+        return compteRepository.save(compte);
+    }
+
+    @PutMapping(path = "/comptes/{code}")
+    public Compte update(@PathVariable Long code, @RequestBody Compte compte) {
+        compte.setCode(code);
+        return compteRepository.save(compte);
+    }
+
+    @DeleteMapping(path = "/comptes/{code}")
+    public void delete(@PathVariable Long code) {
+        compteRepository.deleteById(code);
+    }
 }
